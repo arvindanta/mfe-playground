e7a0d8d13d696b6cd560802a9c81088b0ee08072
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-05-17 13:16:11 +0530

    chore(lint): lint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,68 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'standard',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'only-multiline'],
+    'default-case': 'error',
+    'import/extensions': ['warn'],
+    'import/no-extraneous-dependencies': ['error'],
+    'indent': [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        flatTernaryExpressions: true,
+        MemberExpression: 1,
+        VariableDeclarator: 1,
+        ignoredNodes: ['ConditionalExpression'],
+      },
+    ],
+    'jsx-a11y/anchor-is-valid': 'error',
+    'linebreak-style': 'warn',
+    'multiline-ternary': ['off', 'always-multiline'],
+    'new-cap': 'off',
+    'no-console': [
+      'error',
+      { allow: ['clear', 'info', 'error', 'dir', 'trace'] },
+    ],
+    'no-debugger': 'error',
+    'no-implicit-globals': 'error',
+    'no-restricted-globals': 'error',
+    'no-var': 'error',
+    'prettier/prettier': 'error',
+    'quotes': [1, 'single', { avoidEscape: true }],
+    'quote-props': ['error', 'consistent'],
+    'react/jsx-uses-vars': 'error',
+    'react/require-default-props': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-no-literals': ['error'],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'semi': ['error', 'always'],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'ignore',
+        named: 'ignore',
+        asyncArrow: 'always',
+      },
+    ],
+  },
+  plugins: ['react', 'react-hooks', 'import', 'jsx-a11y', 'prettier'],
   settings: {
     react: {
       version: 'detect',
     },
   },
-  // parser: '@babel/eslint-parser',
+  //  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
--- packages/react-mfe/src/index.js
+++ packages/react-mfe/src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var x = 1;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
-      </BrowserRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
