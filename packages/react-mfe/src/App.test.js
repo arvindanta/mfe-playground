9d7cfada5bb4c421b0eea76851a3e7839c4f533d
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-05-20 09:38:16 +0530

    test(test-router): add memory router to test cases

--- packages/react-mfe/src/App.test.js
+++ packages/react-mfe/src/App.test.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   expect(2).toEqual(2);
 });
