c8b43a66b25c67dcc0f945c505eff4d793414d52
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-05-26 11:51:58 +0530

    feat(stencil-mfe): mfe setup stencil

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,4 +76,43 @@ module.exports = {
     amd: true,
     node: true,
   },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest', // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+        useJSXTextNode: true,
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: './tsconfig.json',
+      },
+      env: {
+        browser: true,
+        jest: true,
+        node: true,
+      },
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended',
+      ],
+      plugins: ['@typescript-eslint', 'jsx-a11y'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 0,
+        'react/no-jsx-bind': 0,
+        'jsx-quotes': [1, 'prefer-single'],
+        'eqeqeq': 2,
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-empty-function': ['off'],
+        'react/jsx-filename-extension': [
+          1,
+          { extensions: ['.js', '.jsx', '.tsx', '.ts'] },
+        ],
+      },
+    },
+  ],
 };
