2d7f58b18a294a915d85792cdf6b43c219037e81
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-05-31 12:57:09 +0530

    chore(sidebar menus): updated sidebar menus

--- packages/react-mfe-shell/src/components/Sidebar/menu.config.js
+++ packages/react-mfe-shell/src/components/Sidebar/menu.config.js
@@ -118,38 +118,4 @@ export const sideMenu = [
     Icon: 'agent',
     to: '/stencilmfe1',
   },
-  {
-    label: 'About',
-    Icon: 'explore',
-    to: '/about',
-  },
-  {
-    label: 'Settings',
-    Icon: 'alert',
-    to: '/settings',
-    children: [
-      {
-        label: 'About',
-        Icon: 'agent',
-        to: 'account',
-      },
-      {
-        label: 'Security',
-        Icon: 'explore',
-        to: 'security',
-        children: [
-          {
-            label: 'Credentials',
-            Icon: 'cross',
-            to: 'credentials',
-          },
-          {
-            label: '2-FA',
-            Icon: 'browser',
-            to: '2fa',
-          },
-        ],
-      },
-    ],
-  },
 ];
