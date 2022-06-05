b348967ae643b97da568b18fc14daaf0677b046c
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-06-05 16:46:12 +0530

    feat(craco-config): add craco config for both umd and systemjs

--- packages/react-mfe/craco.config.js
+++ packages/react-mfe/craco.config.js
@@ -1,7 +1,15 @@
+const path = require('path');
 module.exports = function () {
   return {
     webpack: {
       configure: (webpackConfig, { env, paths }) => {
+        webpackConfig.output = {
+          ...{
+            filename: 'static/js/main.esm.js',
+            path: path.join(__dirname, 'build'),
+            publicPath: process.env.PUBLIC_URL
+          },
+        };
         return webpackConfig;
       },
     },
